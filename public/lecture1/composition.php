230583b0ff2f2f77a3be08410a5dbb50abadecfe
kodal <kodal@publicplan.de>
2025-01-09 19:23:39 +0100

    demonstrate public/private access

--- public/lecture1/composition.php
+++ public/lecture1/composition.php
@@ -1,24 +1,38 @@
 <?php
 class Engine {
     public function start() {
+        $this->startEngine();
+    }
+    private function startEngine() {
         echo "Engine started\n";
+    }
+}
+class SoundSystem {
+    public function start() {
+        echo "SoundSystem: BOOM BOOM!\n";
     }
 }
 
 class Car {
     private $engine;
+    private $soundSystem;
+    private $carType;
 
-    public function __construct() {
+    public function __construct($carType = "") {
         $this->engine = new Engine();
+        $this->carType = $carType;
+        $this->soundSystem = new SoundSystem();
     }
 
     public function startCar() {
         $this->engine->start();
-        echo "Car is running\n";
+        $this->soundSystem->start();
+        echo "Car ($this->carType) is running\n";
+    }
+    public function stopCar() {
+        echo "Car stops";
     }
 }
-$car = new Car();
+$car = new Car("Convertible");
 $car->startCar();
-// Ausgabe:
-// Engine started
-// Car is running
+$car->stopCar();
